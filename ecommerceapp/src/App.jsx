23c6a78ee860a362677d455a4a44ac3ea32b564b
Christian Pereyra <christianpereyra.ftb@gmail.com>
2023-09-13 16:25:38 -0300

    navbar basspoint

--- ecommerceapp/src/App.jsx
+++ ecommerceapp/src/App.jsx
@@ -1,37 +1,16 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-//import viteLogo from '/vite.svg'
 import './App.css'
 import Button from './components/Button/Button'
+import NavBar from './components/navBar/navBar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
-    <>
-      <div>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1 style={{color: 'orange', fontSize: 20}}>BASS POINT</h1>
-      <h2 style={{color: 'greenyellow'}}>Productos de Alto Rendimiento</h2>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <Button label={`count is ${count}`} color='violet' onClick={() => setCount((count) => count + 1)} />
-        <Button label='inicio' color='red'  onClick={() => console.log("Inicio")} />
-        <Button label='contactos' color='green'  onClick={() => console.log("Contacto")}/>
-        <Button label ='nosotros' color='blue'  onClick={() =>console.log("Nosotros")}/>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <NavBar/>
+
+    </div>
   )
 }
 
---
+++ ecommerceapp/src/components/navBar/navBar.jsx
@@ -0,0 +1,14 @@
+const NavBar = () => {
+    return(
+        <nav>
+            <h1 style= {{color:"orange"}}>Bass Point</h1>
+            <h2 style={{color:"gold"}}>Vibra a tu ritmo</h2>
+            <div>
+                <button>Auriculares</button>
+                <button>Parlantes</button>
+                <button>Consolas</button>
+            </div>
+        </nav>
+    )
+}
+export default NavBar
